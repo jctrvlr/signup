54679db645d61642a29d6cf45f61f2213441d8fc
jctrvlr <jcummings5@gmail.com>
2016-10-17 18:27:05 -0400

    Removed swp files

--- register.php
+++ register.php
@@ -1,12 +1,14 @@
 <?php
-$first_name = $_POST["first_name"];
-$last_name = $_POST["last_name"];
-$user_name = $_POST["user_name"];
-$email = $_POST["email_1"];
-$passwd = $_POST["password_1"];
+class form1 {
 
-function bCrypt($pass,$cost)
-{
+  protected $first_name = $_POST["first_name"];
+  protected $last_name = $_POST["last_name"];
+  protected $user_name = $_POST["user_name"];
+  protected $email = $_POST["email_1"];
+  protected $passwd = $_POST["password_1"];
+
+  private function bCrypt($pass,$cost)
+  {
 	$chars='./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	// Build the beginning of the salt
 	$salt = sprintf('$2a$%02d$', $cost);
@@ -19,13 +21,20 @@ function bCrypt($pass,$cost)
 
 	// return the hash
 	return crypt($pass,$salt);
-}
-$hash = bCrypt($passwd, 12);
-
-// $passwd_md5 = md5($passwd); 
-$reg_info = array( "first_name" => "$first_name", "last_name" => "$last_name", 
+  }
+  private function create_user($first_name, $last_name, $user_name,
+  		$email, $passwd)
+  {
+    include("../../../cnf.php")
+    $hash = bCrypt($passwd, 12);
+    
+    $db = new mysqli(
+    // $passwd_md5 = md5($passwd); 
+  }
+  $reg_info = array( "first_name" => "$first_name", "last_name" => "$last_name", 
 		"user_name" => "$user_name", "email" => "$email",
 		"passwd" => "$passwd" );
 
 print_r($reg_info);
+}
 ?>

d0b53238947026427ab5967c1be1cbb00b22a108
jctrvlr <jcummings5@gmail.com>
2016-10-18 14:22:54 -0400

    Added some more php code utilizing PDO's

--- register.php
+++ register.php
@@ -1,11 +1,44 @@
 <?php
+include("../../../cnf.php");
+
+class database {
+  private $host = DB_HOST;
+  private $user = DB_USER;
+  private $pass = DB_PASS;
+  private $dbname = DB_NAME;
+  
+  private $dbh;
+  private $error;
+  
+  private $stmt;
+
+  public function __construct() {
+	$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+	// Set options
+	$options = array(
+	  PDO::ATTR_PERSISTENT => true,
+	  PDO::ATR_ERRMODE => PDO::ERRMODE-EXCEPTION
+	);
+	// Create a new PDO instance
+	try{
+	    $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+	}
+	// Catch errors
+	catch(PDOException $e){
+	    $this->error = $e->getMessage();
+	}
+  }
+  public function query($query) {
+	$this->stmt = $this->dbh->prepare($query);
+  }
+}
 class form1 {
 
-  protected $first_name = $_POST["first_name"];
-  protected $last_name = $_POST["last_name"];
-  protected $user_name = $_POST["user_name"];
-  protected $email = $_POST["email_1"];
-  protected $passwd = $_POST["password_1"];
+  private $first_name;
+  private $last_name;
+  private $user_name;
+  private $email;
+  private $passwd;
 
   private function bCrypt($pass,$cost)
   {
@@ -25,16 +58,17 @@ class form1 {
   private function create_user($first_name, $last_name, $user_name,
   		$email, $passwd)
   {
-    include("../../../cnf.php")
+    include("../../../cnf.php");
     $hash = bCrypt($passwd, 12);
     
-    $db = new mysqli(
+    $db = new mysqli($hostname, $username, $password, $project);
     // $passwd_md5 = md5($passwd); 
-  }
-  $reg_info = array( "first_name" => "$first_name", "last_name" => "$last_name", 
-		"user_name" => "$user_name", "email" => "$email",
-		"passwd" => "$passwd" );
-
+  
+    $reg_info = array( "first_name" => "$first_name", "last_name" => "$last_name", "user_name" => "$user_name", "email" => "$email", "passwd" => "$passwd" );
+    return $reg_info; 
+ }
+$obj = new form1;
+echo $obj->create_user();
 print_r($reg_info);
 }
 ?>
